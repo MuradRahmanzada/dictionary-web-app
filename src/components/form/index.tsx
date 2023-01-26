4c49fb787fe1efef3a9a187c83564195a302b7f7
muradrahmanzada <muradrahmanzada@gmail.com>
2023-01-26 13:48:54 +0400

    styled form

--- src/components/form/index.tsx
+++ src/components/form/index.tsx
@@ -1,7 +1,20 @@
 import React from "react";
+import ISearch from "/assets/icon-search.svg";
 
 const Form = () => {
-  return <div>Form</div>;
+  return (
+    <form className="py-14 max-w-2xl">
+      <div className="relative">
+        <input
+          placeholder="Search for the definition of a word"
+          className="relative w-full h-full p-4 outline-none rounded-xl bg-gray-100"
+        />
+        <button type="submit">
+          <img src={ISearch} className="absolute top-4 right-0 mx-5" />
+        </button>
+      </div>
+    </form>
+  );
 };
 
 export { Form };

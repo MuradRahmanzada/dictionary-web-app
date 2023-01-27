40662cdcabeb6dce5282a1b48a3dc2cc6531db3b
muradrahmanzada <muradrahmanzada@gmail.com>
2023-01-27 23:09:26 +0400

    change img path

--- src/components/form/index.tsx
+++ src/components/form/index.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-import ISearch from "/assets/icon-search.svg";
 
 const Form = () => {
   return (
@@ -10,7 +9,10 @@ const Form = () => {
           className="relative w-full h-full p-4 outline-none rounded-xl bg-gray-100"
         />
         <button type="submit">
-          <img src={ISearch} className="absolute top-4 right-0 mx-5" />
+          <img
+            src="/assets/icon-search.svg"
+            className="absolute top-4 right-0 mx-5"
+          />
         </button>
       </div>
     </form>
